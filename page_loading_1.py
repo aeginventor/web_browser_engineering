3e796946b6dbbfcdb7d7e856421099307ef450f4
San <aeginventor@gmail.com>
2025-11-25 23:31:50 +0900

    feat: load, main 함수 구현
    request와 show를 묶은 load 함수
    커맨드 라인에서 실행 시 load를 실행시켜,  첫 번째 인자를 읽어서 URL로
    사용하는 main 함수

--- page_loading_1.py
+++ page_loading_1.py
@@ -51,3 +51,11 @@ def show(body):
             in_tag = False
         elif not in_tag:
             print(c, end="")
+
+def load(url):
+    body = url.request()
+    show(body)
+
+if __name__ == "__main__":
+    import sys
+    load(URL(sys.argv[1]))
